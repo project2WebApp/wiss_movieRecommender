6cd494abfe4844a38614b195f94bd45a1c3fe14b
Javier Agudo <javieragudo3d@gmail.com>
2019-11-29 09:25:56 +0100

    css pirata

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -10,3 +10,19 @@ function go() {
     console.log("Error while getting the data: ", err);
   });
 };
+let swtc = false;
+document.getElementById("navBar-username").onclick = function(event) {
+  event.preventDefault();
+  if(swtc != true){
+    document.getElementById("pop-up-container").className = "pop-container"
+    swtc = true
+    console.log("entrando")
+  }else{
+    document.getElementById("pop-up-container").className = "pop-container hidden"
+    swtc = false
+   
+    console.log("saliendo")
+  }
+}
+  
+ 
